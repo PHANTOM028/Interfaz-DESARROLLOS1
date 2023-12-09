0b42f20a95d058b6c7318ba59de0c8b1dfd6ce62
Erick ce <92422297+PHANTOM028@users.noreply.github.com>
2023-12-09 13:28:18 -0600

    iNTERFAZ
    HOTEL

---
+++ Crowny-Hotel-main/src/Components/gallery/GalleryData.js
@@ -0,0 +1,28 @@
+const GalleryData = [
+  {
+    img: "images/gallery-1.jpg",
+    title: "Destino One",
+  },
+  {
+    img: "images/gallery-2.jpg",
+    title: "Destino Two",
+  },
+  {
+    img: "images/gallery-3.jpg",
+    title: "DestinoThree",
+  },
+  {
+    img: "images/gallery-4.jpg",
+    title: "Destino four",
+  },
+  {
+    img: "images/gallery-5.jpg",
+    title: "Destino Five",
+  },
+  {
+    img: "images/gallery-6.jpg",
+    title: "Destino Six",
+  },
+]
+
+export default GalleryData
